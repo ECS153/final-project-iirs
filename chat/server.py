b0ee4c18a64ab0b4a8e53dbd2b8559e13c437c36
rohitdhamankar <rdhamankar@ucdavis.edu>
2020-05-14 14:19:56 -0700

    code to handle multiple connections, untested with client side

--- chat/server.py
+++ chat/server.py
@@ -10,15 +10,19 @@ Todo:
 - Ensure the temp buffers and sending rounds work
 
 How server could work for our first test (could be different once multi-connection):
-- Connect to source client, send any waiting data, accept any new data 
+- Connect to source client, send any waiting data, accept any new data
 - Store new data in buffer, close.
 - Once dest client connects, send any data waiting for him, etc. Same process.
 """
 
 import socket, ssl
+from threading import Thread
 
 HOST = socket.gethostname() # For testing we will use same machine
 PORT = 12345 # Arbitrary port for connecting
+MSG_SIZE = 4096 # Size of the message sent / received
+
+clients = {} # holds all the active clients
 
 def server():
     # Create the socket instance and bind the host + port
@@ -37,8 +41,8 @@ def server():
 
     while True:
         # Get 4096 byes of data from client
-        data = conn.recv(4096).decode()
-        if not data: 
+        data = conn.recv(MSG_SIZE).decode()
+        if not data:
             break # If we got nothing, then break
         data_str = str(data)
         print("Got data: " + data_str)
@@ -59,7 +63,7 @@ def server():
         elif source_client == "2" and to_usr2:
             print("Checking 2's buffer")
             data_send = to_usr2.pop()
-        
+
         # Send data back to source client
         print("[Server] Sending data: " + data_send)
         conn.sendall(data_send.encode())
@@ -80,6 +84,57 @@ def server():
     # Close the connection
     conn.close()
 
+def accept_connection(server):
+    while True:
+        client, addr = server.accept()
+        print("connected")
+        clients[client] = addr
+        Thread(target=client_thread, args=(client)).start()
+
+def client_thread(client):
+    while True:
+        try:
+            data = client.recv(MSG_SIZE).decode()
+            if not data:
+                continue
+                #remove(conn)
+            else:
+                send_message(data)
+
+        except:
+            continue
+
+def decode_data(data):
+    # Decode the header
+    source_client = data[1:2] # The "address" of source client
+    dest_client = data[3:4] #  The "address" of dest client
+    client_mode = data[5:6] # The mode in which client is (R, X)
+    # Note: R = read, X = read + write
+    message = data[7:]
+    return source_client, dest_client, client_mode, message
+
+def send_message(data):
+    src, dest, mode, message = decode_data(str(data))
+    if not src in clients or not dest in clients:
+        print("invalid src: " + src + " or dest: " + dest)
+        return
+    for client, addr in clients.items():
+        if dest == addr:
+            client.sendall(message.encode())
+
+def setup_server():
+    main_socket = socket.socket()
+    main_socket.bind((HOST,PORT))
+
+    # Listen for connection and accept, printing the status
+    main_socket.listen()
+    conn, addr = main_socket.accept()
+    print("[Server] Connected to " + str(addr))
+    return main_socket
 
 if __name__ == "__main__":
-    server()
+    server = setup_server()
+    connection_thread = Thread(target=accept_connection, args=(server,)).start()
+    connection_thread.join()
+
+    server.close()
