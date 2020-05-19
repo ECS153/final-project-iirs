2f7fef9b1b6f116550a2990b33067d37a0dadc68
Ian Douglas Scott <ian@iandouglasscott.com>
2020-05-18 19:07:48 -0700

    Add entry points to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version="0.0.1",
     install_requires=['cryptography'],
     packages=['iirs', 'iirs.client', 'iirs.server'],
+    entry_points= {
+        'console_scripts': ['iirs-server = iirs.server.main:main'],
+        'gui_scripts': ['iirs-client = iirs.client.main:main']
+    }
 )
