fc0df5ccab7b69a071e809fb3fa18d16c971e1bd
irvinlesh <irvinlesh12@gmail.com>
2020-05-30 00:29:19 -0700

    Final working commit - Added garbage collector - Added round based deaddrop clearance - Minor refactoring and fixes

--- iirs/server/deaddrop.py
+++ iirs/server/deaddrop.py
@@ -3,9 +3,12 @@
 from ..message import Message
 import time
 from typing import List
+from threading import Thread
 
-# Temp global expire limit till figure out what to do with messages
-EXPIRE_LIMIT = 0
+# Global expire limit till figure out what to do with messages
+EXPIRE_LIMIT = 0 # seconds past commit to deaddrop (keep at 0 for [BETA 2] Solution)
+# Global frequency that the garbage collector will run at
+COLLECT_FREQ = 1 # seconds
 
 # Deaddrop Message Wrapper Object
 class DeaddropMessage:
@@ -34,8 +37,20 @@ class DeadDrop:
     
     def delete(self):
         # Deletes all expired messages within a deaddrop
+        old_len = len(self.data)
         cur_time = int(time.time())
-        self.data = [x for x in self.data if ((cur_time - x.timestamp) >= EXPIRE_LIMIT)]                       
+        self.data = [x for x in self.data if ((cur_time - x.timestamp) < EXPIRE_LIMIT)]       
+
+class GarbageCollector(Thread):
+    def __init__(self, deaddrops):
+        Thread.__init__(self)
+        self.dd = deaddrops
+    def run(self):
+        while True:
+            print("Running garbage collector...")
+            for deaddrop in self.dd.values():
+                deaddrop.delete()
+            time.sleep(COLLECT_FREQ)            
 
 # Holds and manages deaddrops
 class DeaddropManager:
@@ -46,31 +61,30 @@ class DeaddropManager:
         for i in range(1,num + 1):
             self.dd[str(i)] = DeadDrop()
         
-        # Implement garbage collection + initialization
-        # Todo
+        # [BETA 1] Start garbage collection (Implemented and working but not turning this on until needed)
+        # (We will be attempting to delete upon every round)
+        #gc = GarbageCollector(self.dd)
+        #gc.start()
 
-    def store(self, messages: List[Message]):
+    def __store(self, messages: List[Message]):
         # Stores a set of data into their respective deaddrops
-        # Returns a list of data blocks that were not able to be saved
-        ret_data = []
+        # Returns a list of data blocks that were not handled, and the contents of deaddrops
+        failures = []
         store_order = []
-
         for msg in messages:
             try:
-                # Get message dest, store in DD, store dest in store_order
+                # Get message dest, store in DD, return the order of deaddrops
                 cur_key = msg.dest
                 self.dd[cur_key].insert(msg)
                 store_order.append(cur_key)
             except KeyError:
                 print("[Server] Deaddrop storage error. Invalid key.")
                 print("Offending message:", msg.body)
-                ret_data.append(msg)
+                failures.append(msg)
 
-        return ret_data, store_order
+        return failures,store_order
     
-    def get(self, ids: List[str]):
-         # Todo: I will rework this to not send a user's message back to that same user
-
+    def __get(self, ids: List[str]):
         # Gets all the messages in the deaddrops provided in ids and returns
         ret_data = []
         for i in ids:
@@ -79,12 +93,15 @@ class DeaddropManager:
             except KeyError:
                 print("[Server] Deaddrop extraction error. Invalid key.")
                 print("Offending key:", i)
+        # [BETA 2] Once we get all deaddrops, delete the data
+        for i in self.dd.values():
+            i.delete()
         # Returns an empty list if no messages to send
         return ret_data
 
     def handle_messages(self, messages):
         # Store the messages recieved
-        not_stored, store_order = self.store(messages)
+        not_stored, store_order = self.__store(messages)
         # Get the contents of the deaddrops and put in list
-        retrieved_messages = self.get(store_order)
+        retrieved_messages = self.__get(store_order)
         return retrieved_messages
