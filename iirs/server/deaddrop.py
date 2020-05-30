49ab863824bab3caaf5dd70caea20dde62d0f9f4
irvinlesh <irvinlesh12@gmail.com>
2020-05-29 18:52:30 -0700

    Working commit of deaddrop system

--- iirs/server/deaddrop.py
+++ iirs/server/deaddrop.py
@@ -1,10 +1,45 @@
-from..message import *
 # Deaddrop implementation for the server
+
+from ..message import Message
+import time
+from typing import List
+
+# Temp global expire limit till figure out what to do with messages
+EXPIRE_LIMIT = 0
+
+# Deaddrop Message Wrapper Object
+class DeaddropMessage:
+    def __init__(self, message: Message):
+        # Set the time stamp and store the message
+        self.timestamp = int(time.time())
+        self.data = message
+
+# Main Deaddrop Object
+class DeadDrop:
+    def __init__(self):
+        # Create an empty list for storage
+        self.data = []
+    
+    def insert(self, message):
+        # Inserts a message into the deaddrop
+        self.data.append(DeaddropMessage(message))
+    
+    def get(self):
+        # Returns all the messages within the current deaddrop
+        raw_data = []
+        # Remove DeaddropMessage class wrapper
+        for block in self.data:
+            raw_data.append(block.data)
+        return raw_data
+    
+    def delete(self):
+        # Deletes all expired messages within a deaddrop
+        cur_time = int(time.time())
+        self.data = [x for x in self.data if ((cur_time - x.timestamp) >= EXPIRE_LIMIT)]                       
 
 # Holds and manages deaddrops
 class DeaddropManager:
     def __init__(self, num):
-        # Initialization code will go here
         # Set up 'num' deaddrops and automatic garbage collection
         self.dd = {}
         self.num = num
@@ -12,47 +47,44 @@ class DeaddropManager:
             self.dd[str(i)] = DeadDrop()
         
         # Implement garbage collection + initialization
+        # Todo
 
-    #message here is of the message class
-    def store(self, messages):
-
-        ret_data = []
+    def store(self, messages: List[Message]):
         # Stores a set of data into their respective deaddrops
         # Returns a list of data blocks that were not able to be saved
-        return ret_data, messages
-    
-    def get(self, messages):
         ret_data = []
-        # Returns all the contents of a certain deaddrop.
-        # Returns an empty list if empty or not found.
-        return ret_data, messages
+        store_order = []
 
-    # a high level idea of how the dead drop should interact with the mix net
-    # the message class contains a dest, this will be the dead drop it should go to
+        for msg in messages:
+            try:
+                # Get message dest, store in DD, store dest in store_order
+                cur_key = msg.dest
+                self.dd[cur_key].insert(msg)
+                store_order.append(cur_key)
+            except KeyError:
+                print("[Server] Deaddrop storage error. Invalid key.")
+                print("Offending message:", msg.body)
+                ret_data.append(msg)
+
+        return ret_data, store_order
+    
+    def get(self, ids: List[str]):
+         # Todo: I will rework this to not send a user's message back to that same user
+
+        # Gets all the messages in the deaddrops provided in ids and returns
+        ret_data = []
+        for i in ids:
+            try: 
+                ret_data.append(self.dd[i].get())
+            except KeyError:
+                print("[Server] Deaddrop extraction error. Invalid key.")
+                print("Offending key:", i)
+        # Returns an empty list if no messages to send
+        return ret_data
+
     def handle_messages(self, messages):
-        # store the message where they belong
-        stored_messages = self.store(messages)
-        # now retrieve messages, basically go to the same dead drops you just dropped them off at
-        retrieved_messages = self.get(stored_messages)
-        # it is very import that the message in messages[i] which go to dead drop b
-        # is then filled like messages[i] = out_going_messages_from_deaddrop(b)
-        # the index must be the same so I can reverse the shuffle
+        # Store the messages recieved
+        not_stored, store_order = self.store(messages)
+        # Get the contents of the deaddrops and put in list
+        retrieved_messages = self.get(store_order)
         return retrieved_messages
-
-# Main class to be used for deaddrop objects
-class DeadDrop:
-    def __init__(self):
-        # Initialization code will go here
-        pass
-    
-    def insert(self, message):
-        # Inserts a message into the deaddrop
-        pass
-    
-    def get(self):
-        # Returns all the messages within the current deaddrop
-        pass
-    
-    def delete(self):
-        # Deletes all expired messages within a deaddrop
-        pass
